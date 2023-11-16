b50680f0d6a331883731225484e64eb295bb0919
ejt1 <github@wector.info>
2023-11-16 08:36:59 +0100

    update sdk

--- sdk/types/filesystem.d.ts
+++ sdk/types/filesystem.d.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 declare module "fs" {
+  /** @deprecated since 1.9.6 */
   declare class Path {
     constructor(path: String);
 
@@ -7,22 +8,46 @@ declare module "fs" {
     isRegularFile: Boolean;
   }
 
+  /** @deprecated since 1.9.6 */
   declare class Directory {
     entries: Array<Path>;
   }
 
   declare class File {
-    close(): void;
+    close(): number;
     flush(): void;
     tell(): number;
+    seek(offset: number, whence: number): number;
     eof(): boolean;
     read(buffer: ArrayBuffer, position: number, length: number): boolean;
     write(buffer: ArrayBuffer, position: number, length: number): boolean;
-    getline(): String;
+    getline(): string;
+    readAsString(maxLength?: number): string;
+    puts(...args: string): void;
+    getByte(): number;
+    putByte(c: number): number;
   }
 
-  function open(filename: Path | String): File | Directory;
-  function remove(filename: Path | String): boolean;
-  function rename(oldname: Path | String, newname: Path | String);
-  function mkdir(path: Path | String): boolean;
+  declare class Dir {
+    path: string;
+
+    close(): void;
+    read(): Dirent | null;
+  }
+
+  declare class Dirent {
+    name: string;
+    path: string;
+
+    isFile(): boolean;
+    isDirectory(): boolean;
+  }
+
+  function open(path: string, mode: string): File?;
+  function opendir(path: string): Dir;
+  function tmpfile(): File?;
+  function exists(path: string): boolean;
+  function rm(path: string, recursive?: boolean): boolean;
+  function rename(oldPath: string, newPath: string): void;
+  function mkdir(path: string): boolean;
 }
